df215bd5d1d5711de005df59c7420751179ff88a
msjahid <msjahid.ai@gmail.com>
2024-11-06 10:50:53 -0500

    Initialize personal site project with basic file structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -125,27 +125,26 @@
     }
   });
 
-  // Porfolio isotope and filter
-  $(window).on('load', function() {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item',
-      layoutMode: 'fitRows'
-    });
-
-    $('#portfolio-flters li').on('click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
-
-      portfolioIsotope.isotope({
-        filter: $(this).data('filter')
-      });
-    });
-
+ // Portfolio isotope and filter
+$(window).on('load', function() {
+  var portfolioIsotope = $('.portfolio-container').isotope({
+    itemSelector: '.portfolio-item',
+    layoutMode: 'fitRows'
   });
 
-  // Initiate venobox (lightbox feature used in portofilo)
-  $(document).ready(function() {
-    $('.venobox').venobox();
+  $('#portfolio-flters li').on('click', function() {
+    $("#portfolio-flters li").removeClass('filter-active');
+    $(this).addClass('filter-active');
+
+    portfolioIsotope.isotope({
+      filter: $(this).data('filter')
+    });
   });
+});
+
+// Initiate venobox (lightbox feature used in portfolio)
+$(document).ready(function() {
+  $('.venobox').venobox();
+});
 
 })(jQuery);
